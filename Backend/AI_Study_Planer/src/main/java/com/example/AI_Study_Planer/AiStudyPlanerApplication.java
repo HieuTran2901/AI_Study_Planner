92a1444889074cccce4e72672496a8ecfc3719c7
Hieutran <trunghieunaruto204@gmail.com>
2026-04-11 15:13:36 +0700

    Implement backend for website

---
+++ Backend/AI_Study_Planer/src/main/java/com/example/AI_Study_Planer/AiStudyPlanerApplication.java
@@ -0,0 +1,13 @@
+package com.example.AI_Study_Planer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStudyPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiStudyPlanerApplication.class, args);
+	}
+
+}
---
+++ Backend/AI_Study_Planer/src/test/java/com/example/AI_Study_Planer/AiStudyPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AI_Study_Planer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStudyPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
