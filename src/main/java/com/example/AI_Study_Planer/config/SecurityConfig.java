b1a513b5a1a1f0c226e116e4d4947363f7eacce0
Hieutran <trunghieunaruto204@gmail.com>
2026-04-12 16:09:25 +0700

    Add jwt filter

--- src/main/java/com/example/AI_Study_Planer/config/SecurityConfig.java
+++ src/main/java/com/example/AI_Study_Planer/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.AI_Study_Planer.config;
 
+import com.example.AI_Study_Planer.constant.RoleName;
+import com.example.AI_Study_Planer.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,10 +12,14 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new  BCryptPasswordEncoder();
@@ -38,9 +45,11 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                        .requestMatchers("/api/student").hasRole(RoleName.STUDENT)
                         .anyRequest().authenticated()
 
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
